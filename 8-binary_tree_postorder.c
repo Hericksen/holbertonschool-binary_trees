2490fb89c5f784dea3e888b146e0e371f52fae3d
Hericksen <gwendal.martin35@hotmail.fr>
2024-12-09 19:24:20 +0100

    Correction of function name

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,16 +6,16 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node.
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
     {
         return;
     }
 
-        binary_tree_preorder(tree->left, func);
+        binary_tree_postorder(tree->left, func);
 
-        binary_tree_preorder(tree->right, func);
+        binary_tree_postorder(tree->right, func);
         
         func(tree->n);
 }
