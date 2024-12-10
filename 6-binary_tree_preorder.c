3feb498b66ad53d9c500f58bbc3e2be470710ee6
Hericksen <gwendal.martin35@hotmail.fr>
2024-12-10 14:07:10 +0100

    Fix bug indentation

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
 * binary_tree_preorder - goes through a binary tree using pre-order traversal
 *
@@ -7,16 +6,13 @@
 * @func: pointer to a function to call for each node
 * The value in the node must be passed as a parameter to this function
 */
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
 
-		func(tree->n);
-
-		binary_tree_preorder(tree->left, func);
-
-		binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 
 }
